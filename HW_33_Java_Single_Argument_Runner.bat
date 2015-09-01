960877828ed24d2d05f236364b24d1953047673a
PFName PLNmae <PFNamePLNmae@gmail.com>
2015-08-31 23:22:28 -0700

    HW_33_Java_Single_Argument

--- HW_33_Java_Single_Argument_Runner.bat
+++
@@ -1,1 +0,0 @@
-HW_33_Java_Single_Argument.bat gpitvl2thw HW_33_Java_Single_Argument 1.3 core.HtmlUnit "http://www.learn2test.net|learn2test.net"
---
+++ HW_33_Java_Single_Argument_Runner.sh
@@ -0,0 +1,1 @@
+. ./HW_33_Java_Single_Argument.sh gpitvl2thw HW_33_Java_Single_Argument 1.3 core.HtmlUnit "http://www.learn2test.net|learn2test.net"
